5ab889335fbc72e6d12b4907266cc92fb7ab35b5
Jason Harris <sirmanlypowers@gmail.com>
2016-12-18 07:58:07 +1100

    Added the main() level code with option processing.

---
+++ target/esp8266thing.py
@@ -0,0 +1,43 @@
+# -----------------------------------------------------------------------------
+"""
+
+SparkFun ESP8266 Thing (ESP8266)
+
+"""
+# -----------------------------------------------------------------------------
+
+import cli
+
+# -----------------------------------------------------------------------------
+
+prompt = 'esp8266thing'
+
+# -----------------------------------------------------------------------------
+
+class target(object):
+  """esp8266thing- SparkFun ESP8266 Thing Board with ESP8266"""
+
+  def __init__(self, ui, dbgio):
+    self.ui = ui
+    self.dbgio = dbgio
+
+    self.menu_root = (
+      ('exit', self.cmd_exit),
+      ('help', self.ui.cmd_help),
+      ('history', self.ui.cmd_history, cli.history_help),
+    )
+
+    self.ui.cli.set_root(self.menu_root)
+    self.set_prompt()
+    self.dbgio.cmd_info(self.ui, None)
+
+  def set_prompt(self):
+    indicator = ('*', '')[self.dbgio.is_halted()]
+    self.ui.cli.set_prompt('%s%s> ' % (prompt, indicator))
+
+  def cmd_exit(self, ui, args):
+    """exit application"""
+    self.dbgio.disconnect()
+    ui.exit()
+
+# -----------------------------------------------------------------------------
